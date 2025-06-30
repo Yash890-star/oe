2a45a421fbbb41089966ba43d6be043211298a2d
Yash890-star <81615944+Yash890-star@users.noreply.github.com>
2025-06-30 15:03:06 +0530

    Delete backend directory

--- backend/src/constants/types/commonTypes.ts
+++
@@ -1,9 +0,0 @@
-export const enum RequestMethod {
-    GET = 'GET',
-    POST = 'POST'
-}
-
-export interface ApiConfig {
-    uri: string;
-    method: RequestMethod;
-}
--- backend/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import dummyRoute from './routes';
-
-const router = Router();
-
-router.use('/', dummyRoute);
-
-export default router;
