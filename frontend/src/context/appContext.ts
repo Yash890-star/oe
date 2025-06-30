55fedeb355ac5f097e7d726e433534a80feabbd2
Yash890-star <81615944+Yash890-star@users.noreply.github.com>
2025-06-30 15:03:14 +0530

    Delete frontend directory

--- frontend/src/context/appContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-import { ContextType } from '../types/commonTypes';
-
-export const AppContext = createContext<{ context: ContextType; setContext: React.Dispatch<React.SetStateAction<ContextType>> }>({
-  context: {},
-  setContext: () => {},
-});
--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}', // include your component files
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
